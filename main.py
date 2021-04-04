57d81a7ffd3cd7596805747549e518d108245764
Pratyay1010 <59337443+Pratyay1010@users.noreply.github.com>
2021-04-04 14:19:38 +0530

    main code completed
    --- Bug fix needed
    --- graphics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+os.system("CLS")
 
 #2d array to store the player position
 board=np.array([[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']])
@@ -29,18 +30,66 @@ while True:
         p=p2
     else:
         p=p1
-        
-    """temp=a[0][0]
-    flag=0
-    for i in range(3):
-        for j in range(3):
-            if(a[i][j]==temp):
-                flag=1
-        if flag==1:
-            print(a[i][0]+" Wins")
-            break"""
 
-    #Draw Statementas
+    #Win Statements
+
+    #Horizontal
+    flag=0
+    win=0
+    for i in range(3):
+        for j in range(1,3):
+            if(board[i][0]==board[i][j] and board[i][0]!=' '):
+                flag=1
+            else:
+                flag=0
+                break
+        if(flag==1):
+            print(board[i][0]," Wins")
+            win=1
+            break
+    if win==1:
+        break
+
+    #Vertical
+    flag=0
+    win=0
+    for i in range(3):
+        for j in range(1,3):
+            if(board[0][i]==board[j][i] and board[0][i]!=' '):
+                flag=1
+            else:
+                flag=0
+                break
+        if(flag==1):
+            print(board[0][i]," Wins")
+            win=1
+            break
+    if win==1:
+        break
+
+    #Diagonal
+    flag=0
+    win=0
+    for i in range(1,3):
+        if(board[0][0]==board[i][i] and board[0][0]!=' '):
+            flag=1
+        elif(board[0][2]==board[0+i][2-i] and board[0][2]!=' '):
+            flag=2
+        else:
+            flag=0
+            break
+    if(flag==1):
+        print(board[0][0]," Wins")
+        win=1
+        break
+    elif(flag==2):
+        print(board[0][2]," Wins")
+        win=1
+        break
+    if win==1:
+        break
+
+    #Draw Statements
     if(" " not in board):
         print("Draw")
         break
